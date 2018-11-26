89840dd20cb4c758be67971a86f1886ed948768d
basilelegal@gmail.com <basilelegal@gmail.com>
2018-11-26 19:36:47 +0100

    Adds Basic Auth

---
+++ SitePartage/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SitePartage.Models
+{
+    public class LoginViewModel
+    {
+        [Key]
+        [Required]
+        [Display(Name = "Identifiant")]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+    }
+}
